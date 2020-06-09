36aa699cf7a6f01874e553b350973f369642b6bd
JM-AFSAnalytics <jm@afsanalytics.com>
2020-06-09 15:24:49 +0200

    v4.0.4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## 4.0.4 - 2020-06-09
+
+- Fixed embedded dashboard access denied error.
+
 ## 4.0.3 - 2020-06-04
 
 - Improved account creation workflow.
--- addfreestats.php
+++ addfreestats.php
@@ -4,7 +4,7 @@
   Plugin Name: AFS Analytics
   Plugin URI: https://www.afsanalytics.com/
   Description: Full featured Web Analytics solution. Easy to use, in addition or as an alternative to google analytics.
-  Version: 4.03
+  Version: 4.04
   Author: AFS Analytics
   Author URI: https://www.afsanalytics.com/
   Text Domain: afsanalytics
@@ -36,7 +36,7 @@ if ( ! class_exists( 'AFSA_Core_Stats_Plugin' ) ) :
 
 
 	define( 'AFSA_DEBUG_MODE', false );
-	define( 'AFSA_MODULE_VERSION', '4.0.3' );
+	define( 'AFSA_MODULE_VERSION', '4.0.4' );
 
 	class AFSA_Core_Stats_Plugin {
 
