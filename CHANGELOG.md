518156b03d041bf765e4f454df6a673fcd8f7043
JM-AFSAnalytics <jm@afsanalytics.com>
2023-08-24 16:11:09 +0200

    v4.20

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## 4.0.20 - 2023-08-24
+
+- Wordpress 6.3 support.
+- WooCommerce 8.0 support.
 
 ## 4.0.19 - 2023-05-28
 
--- addfreestats.php
+++ addfreestats.php
@@ -4,13 +4,13 @@
   Plugin Name: AFS Analytics
   Plugin URI: https://www.afsanalytics.com/
   Description: Full featured Web Analytics solution. Easy to use, in addition or as an alternative to google analytics.
-  Version: 4.19
+  Version: 4.20
   Author: AFS Analytics
   Author URI: https://www.afsanalytics.com/
   Text Domain: afsanalytics
   Domain Path: /languages
   WC requires at least: 1.4.1
-  WC tested up to: 7.7.0
+  WC tested up to: 8.0.2
  */
 
 
@@ -36,7 +36,7 @@ if ( ! class_exists( 'AFSA_Core_Stats_Plugin' ) ) :
 
 
 	define( 'AFSA_DEBUG_MODE', false );
-	define( 'AFSA_MODULE_VERSION', '4.0.19' );
+	define( 'AFSA_MODULE_VERSION', '4.0.20' );
 
 	class AFSA_Core_Stats_Plugin {
 
